f6fb443b3abacd9bc422bdb5c2b0f89e2e19e648
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-04 18:19:15 -0800

    adds create a user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(full_name: params[:full_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+
+    # happy/sad paths
+    if user.save
+      render json: { message: "User created successfully!" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
