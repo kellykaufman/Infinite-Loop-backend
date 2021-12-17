5659a69eabb1a86807499e87ff19384a89d46b8c
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-17 12:23:34 -0800

    adds delete header to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:8080"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put]
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
